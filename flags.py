54953b2d3463e3fa41f838cff8d3da401bb4395e
GretaVik <152508611+GretaVik@users.noreply.github.com>
2023-12-14 01:51:52 +0200

    Update flags.py

--- flags.py
+++ flags.py
@@ -5,7 +5,7 @@ Date: 2023-12-14
 """
 
 a = 5
-b = 10
+b = 12
 c = 3
 d = 4
 e = 5
@@ -13,17 +13,17 @@ a += b
 a -= c
 a *= d
 a /= e
-print("a = ", a)
+print("a=", a)
 
-flag1 = 3 > 2
+flag1 = 3 > 4
 flag2 = 2 < 1
 flag3 = flag1 and flag2
 flag4 = flag1 or flag2
 flag5 = not flag1
-print("flag1 = ", flag1)
-print("flag2 = ", flag2)
-print("flag3 = ", flag3)
-print("flag4 = ", flag4)
-print("flag5 = ", flag5)
+print("flag1= ", flag1)
+print("flag2= ", flag2)
+print("flag3= ", flag3)
+print("flag4= ", flag4)
+print("flag5= ", flag5)
 print(flag1 is True)
 print(flag2 is not False)
